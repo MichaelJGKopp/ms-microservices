6c26a77e06fa34dd85e459a26f4845bb5b079e16
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-02-25 21:25:12 +0100

    change number of default retries with circuitbreaker

--- currency-exchange-service/src/main/java/io/michaeljgkopp/github/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/io/michaeljgkopp/github/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,13 +1,30 @@
 package io.michaeljgkopp.github.microservices.currencyexchangeservice;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
+    private final Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+
         @GetMapping("/sample-api")
+        @Retry(name="sample-api")  // name="default" returns error only after failing three times
+        // define in application.properties: resilience4j.retry.instances.sample-api.max-attempts=5
         public String sampleApi() {
-            return "Sample API";
+
+            logger.info("Calling sample api ...");
+
+            // supposed to cause failure
+            ResponseEntity<String> responseEntity = new RestTemplate().getForEntity(
+                    "http://localhost:88/some-dummy-url", String.class);
+
+            return responseEntity.getBody();
         }
 }
