10b66e5a4d4ec15b1de9dc1c9882ab519c1dbab9
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-02-26 09:37:16 +0100

    control retry wait-duration

--- currency-exchange-service/src/main/java/io/michaeljgkopp/github/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/io/michaeljgkopp/github/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.lang.reflect.Executable;
-
 @RestController
 public class CircuitBreakerController {
 
