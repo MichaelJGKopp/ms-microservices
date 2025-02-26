8742e0029bd3527da3eb0a849b0cd6a54609d45b
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-02-26 12:06:26 +0100

    use RateLimiter to allow 2 requests per 10s period

--- currency-exchange-service/src/main/java/io/michaeljgkopp/github/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/io/michaeljgkopp/github/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,6 +1,7 @@
 package io.michaeljgkopp.github.microservices.currencyexchangeservice;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,22 +14,30 @@ import org.springframework.web.client.RestTemplate;
 public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
+
+    // name="default" returns error only after failing three times else
+    // define in application.properties: resilience4j.retry.instances.sample-api.max-attempts=5
+    // @Retry(name="sample-api", fallbackMethod = "hardCodedResponse")
+
     // the circuit breaker trips if failure percentage is higher than threshold
     // it then transitions from closed to open, then it fails fast until a recovery time when it becomes half-open
     // and finally closes again
-    @CircuitBreaker(name="sample-api", fallbackMethod = "hardCodedResponse")
-    // @Retry(name="sample-api", fallbackMethod = "hardCodedResponse")
-    // name="default" returns error only after failing three times
-    // define in application.properties: resilience4j.retry.instances.sample-api.max-attempts=5
+    // @CircuitBreaker(name="sample-api", fallbackMethod = "hardCodedResponse")
+
+    // not more than 10_000 calls per 10s to the sample-api
+    @RateLimiter(name="default")
+
     public String sampleApi() {
 
         logger.info("Calling sample api ...");
 
         // supposed to cause failure
-        ResponseEntity<String> responseEntity = new RestTemplate().getForEntity(
-                "http://localhost:88/some-dummy-url", String.class);
+//        ResponseEntity<String> responseEntity = new RestTemplate().getForEntity(
+//                "http://localhost:88/some-dummy-url", String.class);
+//
+//        return responseEntity.getBody();
 
-        return responseEntity.getBody();
+        return "sample api";
     }
 
     private final Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
